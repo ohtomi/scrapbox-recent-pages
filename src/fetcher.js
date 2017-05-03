11b3d22cf6d17dc5bf47203fc94a93816f86b5f1
ohtomi <ohtomi.kenichi@gmail.com>
2017-05-03 10:57:32 +0900

    Rename private members

--- src/fetcher.js
+++ src/fetcher.js
@@ -8,35 +8,34 @@ class Fetcher {
         this.project = project;
         this.checkIntervalSec = checkIntervalSec;
 
-        this.timer = null;
-
-        this.fetching = false;
+        this._timer = null;
+        this._fetching = false;
         this.cache = [];
         this.error = null;
     }
 
     start() {
-        if (this.timer) {
+        if (this._timer) {
             throw new Error('already started');
         }
 
-        this.timer = setInterval(() => {
-            if (!this.fetching) {
+        this._timer = setInterval(() => {
+            if (!this._fetching) {
                 this.doFetch(0, SCRAPBOX_FETCH_LIMIT, []);
             }
         }, this.checkIntervalSec * 1000);
     }
 
     stop() {
-        if (this.timer) {
-            clearInterval(this.timer);
-            this.timer = null;
+        if (this._timer) {
+            clearInterval(this._timer);
+            this._timer = null;
         }
     }
 
     doFetch(skip, limit, pages) {
 
-        this.fetching = true;
+        this._fetching = true;
         window.fetch(this.baseUrl + '/api/pages/' + this.project + '?skip=' + skip + '&sort=updated&limit=' + limit + '&q=', SCRAPBOX_FETCH_OPTIONS)
             .then(res => {
                 res.json()
@@ -44,14 +43,14 @@ class Fetcher {
                         if (body.skip + body.limit < body.count) {
                             this.doFetch(skip + limit, limit, pages.concat(body.pages));
                         } else {
-                            this.fetching = false;
+                            this._fetching = false;
                             this.cache = pages.concat(body.pages);
                             this.error = null;
                         }
                     });
             })
             .catch(e => {
-                this.fetching = false;
+                this._fetching = false;
                 this.cache = [];
                 this.error = e;
             });
