ef4060bb43f969ea13084bc411a2d1b51779ed5a
ohtomi <ohtomi.kenichi@gmail.com>
2017-05-04 07:09:00 +0900

    Add new class checking whether or not fetched pages were unread

---
+++ src/watcher.js
@@ -0,0 +1,61 @@
+class Watcher {
+
+    constructor(watchesRef, fetchersRef, callbackRef, checkIntervalSec) {
+        this.watchesRef = watchesRef;
+        this.fetchersRef = fetchersRef;
+        this.callbackRef = callbackRef;
+        this.checkIntervalSec = checkIntervalSec;
+
+        this._timer = null;
+        this.cache = [];
+    }
+
+    start() {
+        if (this._timer) {
+            throw new Error('already started');
+        }
+
+        this._timer = setInterval(() => {
+            if (!this._fetching) {
+                this.doWatch();
+            }
+        }, this.checkIntervalSec * 1000);
+    }
+
+    stop() {
+        if (this._timer) {
+            clearInterval(this._timer);
+            this._timer = null;
+        }
+    }
+
+    doWatch() {
+
+        let watches = this.watchesRef();
+        let fetchers = this.fetchersRef();
+        let allPages = fetchers
+            .map(fetcher => {
+                return fetcher.cache;
+            })
+            .reduce((a, b) => {
+                return a.concat(b);
+            });
+
+        this.cache = allPages.filter((element, index, array) => {
+            let baseUrl = element.baseUrl;
+            let project = element.project;
+            let title = element.title;
+            let updated = element.updated;
+
+            let watch = watches.find((element, index, array) => {
+                return element.baseUrl === baseUrl && element.project === project && element.title === title;
+            });
+
+            return watch && (!watch.accessed || watch.accessed < updated);
+        });
+
+        this.callbackRef(this.cache.length);
+
+    }
+
+}
